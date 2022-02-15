a26ee186a04db1fa73d035a7a4b07fc221414674
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-02-15 09:30:55 +0600

    All Issue solved

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,17 @@
+/* ================== Get All Common Variables ================== */
+const subTotal = document.getElementById("sub-total");
+const tax = document.getElementById("tax");
+const total = document.getElementById("total");
+
 /* ================== Phone Plus Button ================== */
 function phonePlus() {
   updateProduct("phone", true, 1219);
 }
 
-
 /* ================== Phone Minus Button ================== */
 function phoneMinus() {
   updateProduct("phone", false, 1219);
 }
-
-
 
 /* ================== Case Plus Button ================== */
 function casePlus() {
@@ -26,33 +28,28 @@ function updateProduct(product, isAdd, price) {
   const productInput = document.getElementById(product + "-input");
   const productDisplay = document.getElementById(product + "-display");
   let productValue = productInput.value;
-  if(isAdd){
-    productValue++
-  } else if(productValue > 0){
-    productValue--
+  if (isAdd) {
+    productValue++;
+  } else if (productValue > 0) {
+    productValue--;
   }
   productInput.value = productValue;
   productDisplay.innerText = parseInt(productValue) * price;
   updateTotal();
 }
 
-
 /* ================== Common Function - Update Product Total ================== */
-function updateTotal(){
-  const subTotal = document.getElementById("sub-total")
-  const tax = document.getElementById("tax")
-  const total = document.getElementById("total")
-
-  const phoneTotal = parseInt(document.getElementById("phone-input").value) * 1219;
-  const caseTotal = parseInt(document.getElementById("case-input").value) * 59;
+function updateTotal() {
+  const phoneTotal = getInteger("phone") * 1219;
+  const caseTotal = getInteger("case") * 59;
 
   subTotal.innerText = phoneTotal + caseTotal;
-  tax.innerText = parseInt(subTotal.innerText) / 10
-  total.innerText = parseFloat(subTotal.innerText) + parseFloat(tax.innerText);
+  tax.innerText = parseFloat(subTotal.innerText) / 10;
+  total.innerText = parseFloat(subTotal.innerText) + parseInt(tax.innerText);
 }
 
 /* ================== Common Function - Get Inner Integer Value ================== */
-function getInteger(prduct){
-  let productInt = parseInt(document.getElementById(prduct+"-input").value)
+function getInteger(prduct) {
+  let productInt = parseInt(document.getElementById(prduct + "-input").value);
   return productInt;
 }
